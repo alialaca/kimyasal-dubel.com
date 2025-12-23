80a11bd4d6a34549cecf308c7fd0240c23367709
alialaca <alialaca0@gmail.com>
2025-12-23 13:56:25 +0300

    Add head configuration and title to Nuxt app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
 
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  }
+    app: {
+        pageTransition: {name: 'page', mode: 'out-in'},
+        head: {
+            title: 'Bossong',
+            meta: [
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {hid: 'description', name: 'description', content: 'Bossong yapı kimyasalları'}
+            ]
+        }
+    }
 })
